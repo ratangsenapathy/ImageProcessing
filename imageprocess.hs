655f2f060ba4e47552994f2fd13de112765dc153
ratansen <ratangsenapathy@gmail.com>
2016-10-03 10:54:33 -0700

    Change the mplementations to make a function apply trnaform and make functions to apply to each pixel like gammaTransformPixel, logTransformPixel and negatePixel

--- imageprocess.hs
+++ imageprocess.hs
@@ -1,35 +1,53 @@
 import System.Environment
 import Codec.Picture
 
+
 type OutputImage = Image PixelRGB8
 type InputImage = Image PixelRGB8
 type Constant = Float
-type LogBase = Float
-type GammaConstant = Float
+type GammaValue = Float
+
 
 negatePixel :: PixelRGB8 -> PixelRGB8
 negatePixel (PixelRGB8 r g b) = PixelRGB8 (255-r) (255-g) (255-b)
 
+
+--function takes a pixel and a constant to use in the formula c*log(1+pixelvalue) where log base is 256
+logTransformPixel :: (Floating constant, RealFrac constant) => constant -> PixelRGB8 -> PixelRGB8
+logTransformPixel const (PixelRGB8 r g b) = PixelRGB8 (log256 r) (log256 g) (log256 b)
+                                            where log256 x = floor (const * logBase 256 (1 + fromIntegral x)) :: Pixel8
+
+
+gammaTransformPixel :: (Floating a, RealFrac a) => a -> a -> PixelRGB8 -> PixelRGB8
+gammaTransformPixel const gammaValue (PixelRGB8 r g b) = PixelRGB8 (gammaTransform r) (gammaTransform g) (gammaTransform b)
+                                                         where gammaTransform x = floor (const * ((fromIntegral x/256) ** gammaValue) * 256) ::Pixel8
+
+
+applyTransform = pixelMap
+
+
 negateImage :: InputImage -> OutputImage
-negateImage x = pixelMap negatePixel x
+negateImage = applyTransform negatePixel
 
-logTransformImage :: (Floating a, RealFrac a) => InputImage -> Constant -> LogBase -> OutputImage
-logTransformImage x c b = pixelMap (\(PixelRGB8 r g b) -> PixelRGB8 (lg (fromIntegral r)) (lg (fromIntegral g)) (lg (fromIntegral b))) x
-                          where lg y =  floor (c * (logBase b (1+y))) :: Pixel8
 
-gammaTransformImage :: (Floating a, RealFrac a) => InputImage -> Constant -> GammaConstant -> OutputImage
-gammaTransformImage x c g =  pixelMap (\(PixelRGB8 r g b) -> PixelRGB8 (gamma (fromIntegral r)) (gamma (fromIntegral g)) (gamma (fromIntegral b))) x
-                             where gamma p =  floor (c * (p**g)) :: Pixel8
-                                
+logTransformImage :: Constant -> InputImage -> OutputImage
+logTransformImage const = applyTransform (logTransformPixel const) 
+                         
+
+gammaTransformImage :: Constant -> GammaValue -> InputImage -> OutputImage
+gammaTransformImage const gammaValue = applyTransform (gammaTransformPixel const gammaValue)
+
+                                          
 extractImage :: Either String DynamicImage -> Image PixelRGB8
 extractImage (Left errorMessage) = error errorMessage
 extractImage (Right dynamicImage) = convertRGB8 dynamicImage
+
 
 main :: IO()
 main = do
   args <-getArgs
   dynamicInput <- readImage (args!!0)
   let inputImage = extractImage(dynamicInput)
-  let resultImage = gammaTransformImage inputImage 1 2
+  let resultImage = gammaTransformImage inputImage 1 0.5
   savePngImage (args!!1) (ImageRGB8 resultImage)
               
